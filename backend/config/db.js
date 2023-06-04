157a7472a87ed6f791f1a82a6575d40d4d125174
Prakhar Sinha <prakhar.sinha2k2@gmail.com>
2023-06-05 00:23:13 +0530

    Connection to Cloud Mongodb Database (Atlas)

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose')
+
+const connectDB=async()=>{
+    try{
+        const conn=await mongoose.connect("mongodb+srv://prakharsinha2k2:O0dr1Bw5PntHDaLE@cluster0.hv5lb1h.mongodb.net/?retryWrites=true&w=majorityO0dr1Bw5PntHDaLE",{
+            useNewUrlParser: true,
+            useUnifiedTopology:true,
+        })
+        console.log(`MongoDB Connected : ${conn.connection.host}`)
+    }
+    catch(error){
+        console.log(`Error :${error.message}`)
+        process.exit() 
+    } 
+}
+
+module.exports=connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express=require('express')
 const dotenv=require('dotenv')
 const chats = require('./data/data')
+const connectDB = require('./config/db')
 
+dotenv.config({path:+'/.env'}) 
+connectDB()
 const app=express()
-dotenv.config()
 
 app.get("/",(req,res)=>{
     res.send("API is running!")
